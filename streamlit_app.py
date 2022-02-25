63395dffcf0c6fc8fc34c8b9466ea6a8d9430e17
akhurana95 <aditya.khurana1@gmail.com>
2022-02-25 16:22:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import joblib
 
 # Title
 st.header("Streamlit Machine Learning App")
@@ -18,16 +17,7 @@ eyes = st.selectbox("Select Eye Colour", ("Blue", "Brown"))
 # If button is pressed
 if st.button("Submit"):
     
-    # Unpickle classifier
-    clf = joblib.load("clf.pkl")
     
-    # Store inputs into dataframe
-    X = pd.DataFrame([[height, weight, eyes]], 
-                     columns = ["Height", "Weight", "Eyes"])
-    X = X.replace(["Brown", "Blue"], [1, 0])
-    
-    # Get prediction
-    prediction = clf.predict(X)[0]
     
     # Output prediction
-    st.text(f"This instance is a {prediction}")
+    st.text(f"This instance is a submission button")
